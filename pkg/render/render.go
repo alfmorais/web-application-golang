0f10432c20127ca74038fb3a7b82408d8f86d834
alfmorais <alfredneto@hotmail.com>
2025-10-05 23:08:41 -0300

    refactor: using base template to extends base html config

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 )
 
+const baseLayoutTemplate string = "./templates/base.layout.tmpl"
+
 func RenderTemplate(w http.ResponseWriter, tmplDir string) {
-	parsedTemplate, _ := template.ParseFiles(tmplDir)
+	parsedTemplate, _ := template.ParseFiles(tmplDir, baseLayoutTemplate)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		log.Println("error parsing template:", err)
